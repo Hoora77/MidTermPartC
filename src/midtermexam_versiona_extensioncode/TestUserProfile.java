4cc79bbf5828187b8315e18f299c71e389fe900d
Hoora77 <houra.tavana@gmail.com>
2021-02-26 20:05:56 +0500

    Add test method for user profile

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,8 @@
+package midtermexam_versiona_extensioncode;
+
+public class TestUserProfile {
+    public static void main(String[] args) {
+        UserProfile profile = new UserProfile("Hoora","Comedy");
+        System.out.println("Your profile is created");
+    }
+}
